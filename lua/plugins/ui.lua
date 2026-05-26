77b9de69a2e12b5f3d3a217fdf66477e764ad1cd
Ye Lin Ko <yelinko.2002@gmail.com>
2026-05-26 15:53:47 +0630

    Hide Git metadata from sidebar

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -88,6 +88,9 @@ return {
           visible = true,
           hide_dotfiles = false,
           hide_gitignored = false,
+          never_show = {
+            ".git",
+          },
         },
       },
       buffers = {
